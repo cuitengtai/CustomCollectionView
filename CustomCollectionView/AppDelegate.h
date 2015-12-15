c4bfa0950775e3820bc4753ea25bbb36c9eecf63
tengtai <cuitengtai@jd.com>
2015-12-15 15:45:04 +0800

    Initial Commit

---
+++ CustomCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCollectionView
+//
+//  Created by 光之晨曦 on 15/12/15.
+//  Copyright © 2015年 光之晨曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCollectionView
+//
+//  Created by 光之晨曦 on 15/12/15.
+//  Copyright © 2015年 光之晨曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
